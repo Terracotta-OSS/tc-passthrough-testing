2c8d7566570a3569a4f9b11660eb728d29b01994
Myron Scott <myron.scott@softwareag.com>
2017-07-25 20:35:01 -0700

    Fit new API

--- passthrough-server/src/main/java/org/terracotta/passthrough/PassThroughServerInvokeContext.java
+++ passthrough-server/src/main/java/org/terracotta/passthrough/PassThroughServerInvokeContext.java
@@ -1,9 +1,10 @@
 package org.terracotta.passthrough;
 
+import org.terracotta.entity.ActiveInvokeContext;
 import org.terracotta.entity.ClientDescriptor;
-import org.terracotta.entity.InvokeContext;
+import org.terracotta.entity.ClientSourceId;
 
-public class PassThroughServerInvokeContext implements InvokeContext {
+public class PassThroughServerInvokeContext implements ActiveInvokeContext {
   private final ClientDescriptor descriptor;
   private final long current;
   private final long oldest;
@@ -32,6 +33,26 @@ public class PassThroughServerInvokeContext implements InvokeContext {
   @Override
   public boolean isValidClientInformation() {
     return current >= 0;
+  }
+
+  @Override
+  public ClientSourceId getClientSource() {
+    return descriptor.getSourceId();
+  }
+
+  @Override
+  public ClientSourceId makeClientSourceId(long opaque) {
+    return new ClientSourceId() {
+      @Override
+      public long toLong() {
+        return opaque;
+      }
+
+      @Override
+      public boolean matches(ClientDescriptor cd) {
+        return cd.getSourceId().toLong() == opaque;
+      }
+    };
   }
   
   
